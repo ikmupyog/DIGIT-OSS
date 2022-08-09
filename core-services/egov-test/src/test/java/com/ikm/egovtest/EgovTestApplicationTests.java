c38a00b5c5369979d52d5ee119ed7f108dc1b61d
Vipin Madhummal <vipinmadhummal@Vipins-MacBook-Pro.local>
2022-08-09 15:25:08 +0530

    new test build

---
+++ core-services/egov-test/src/test/java/com/ikm/egovtest/EgovTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikm.egovtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgovTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
